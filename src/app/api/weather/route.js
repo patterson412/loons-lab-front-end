e92a73dc931349022da1f51ce634f4ccf0546485
patterson412 <patterson.leon1960@gmail.com>
2024-08-27 16:22:09 +0530

    comment changes

--- src/app/api/weather/route.js
+++ src/app/api/weather/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import axios from 'axios';
 
 export async function GET(request) {
-    const { searchParams } = request.nextUrl; // outside the try catch block to prevent DynamicServerError error going to catch block because the route handler in Next is static
+    const { searchParams } = request.nextUrl; // outside the try-catch block, allowing Next.js to detect the dynamic usage early. This prompts Next.js to opt the route into dynamic rendering automatically.
     try {
 
         const latitude = searchParams.get('lat');
